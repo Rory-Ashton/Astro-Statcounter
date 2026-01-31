c94565911eccbe836b9484d632c13cd6249dd34f
Rory Ashton <rory.ashton@gmail.com>
2026-01-31 07:23:45 +0000

    CMP logic complete and tested

--- full.sh
+++ full.sh
@@ -2,17 +2,11 @@
 set -e
 
 cd /var/www/html/astroStatcounter
-rm -f *.tgz
 npm pack
 
 cd /var/www/html/astro
 npm remove @statcounter/astro
-rm -f package-lock.json
-rm -rf node_modules
-npm cache clean --force
-npm install
-npm install /var/www/html/astroStatcounter/statcounter-astro-0.1.2.tgz --force
+npm install /var/www/html/astroStatcounter/statcounter-astro-0.1.2.tgz
 
-rm -rf node_modules/.vite .astro dist
 npm run build
 pm2 restart astro
